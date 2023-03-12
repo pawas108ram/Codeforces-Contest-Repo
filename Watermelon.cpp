555824a3de0b1ac88be2c6c9353de9a372d38fd3
pawas goyal <pawas48ram@gmail.com>
2023-03-12 20:40:10 +0530

    changed

--- Watermelon.cpp
+++ Watermelon.cpp
@@ -4,10 +4,9 @@ int main(){
     int n;
     cin>>n;
     if(n<=2)cout<<"NO"<<endl;
-    if(n%2!=0){
-        cout<<"NO"<<endl;
-    }else{
-        if(n/2%2!=0)
-    cout<<"NO"<<endl;}
-    cout<<"YES"<<endl;
+    else if(n%2==0){
+        cout<<"YES"<<endl;
+    }
+    else{cout<<"NO"<<endl;}
+    return 0;
 }
