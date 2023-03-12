3da8b7425b154af4c83d240f6f15cd999adff8f2
pawas goyal <pawas48ram@gmail.com>
2023-03-12 22:16:15 +0530

    added new ques

---
+++ number theory/catcycle.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        k--;
+        if(n%2==0){
+            cout<<1+(k)%n<<endl;
+        }
+        else{
+            ll ins=(k)/((n-1)/2);
+            cout<<1+(k+ins)%n<<endl;
+        }
+    }
+}
