99c7b16df88be07dc29821ce1e9c5cb3ea824e7f
pawas goyal <pawas48ram@gmail.com>
2023-03-12 18:50:53 +0530

    changed the solution

--- Watermelon.cpp
+++ Watermelon.cpp
@@ -4,8 +4,10 @@ int main(){
     int n;
     cin>>n;
     if(n<=2)cout<<"NO"<<endl;
-    if(n%2==0){
+    if(n%2!=0){
         cout<<"YES"<<endl;
     }else{
+        if(n/2%2!=0)
     cout<<"NO"<<endl;}
+    cout<<"YES"<<endl;
 }
